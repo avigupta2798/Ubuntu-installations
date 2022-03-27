5876223f966ad0dd69b22ad301d12e79fa88ff59
Avi Gupta <32445591+avigupta2798@users.noreply.github.com>
2022-03-28 00:32:26 +0530

    Update installation-script.sh

--- installation-script.sh
+++ installation-script.sh
@@ -15,6 +15,9 @@ sudo dpkg -i zoom_amd64.deb
 sudo add-apt-repository ppa:umang/indicator-stickynotes -y
 sudo apt-get update -y
 sudo apt-get install indicator-stickynotes -y
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
+sudo apt-get update -y
+sudo apt install qbittorrent -y
 sudo apt install python3-pip -y
 sudo apt-get install git -y
 python3 -m pip install --user virtualenv
